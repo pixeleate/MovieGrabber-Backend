9c1a3e12a893684d56fc2e9ea789dd1d2e75b599
Promises <henning.ber@gmail.com>
2017-11-26 00:34:02 +0100

    Initial Commit

---
+++ MovieGrabber/urls.py
@@ -0,0 +1,31 @@
+"""MovieGrabber URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include
+from django.contrib import admin
+import notifications.urls
+from django.views.generic import TemplateView
+
+import LinkGrabberDjango.urls as LinkGrabUrls
+
+urlpatterns = LinkGrabUrls.urlpatterns + [
+                  url(r'^admin/', admin.site.urls),
+                  url('^inbox/notifications/', include(notifications.urls, namespace='notifications')),
+                  url(r'^comments/', include('django_comments.urls')),
+    url(r'^searchaskforapi/', TemplateView.as_view(template_name="FUCK OFF.html"), name='post_list'),
+
+    url(r'^.*', TemplateView.as_view(template_name="angular_home.html"),name="angular"),
+
+              ]
